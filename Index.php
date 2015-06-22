6ed007e3bec52135e9b0eb8e0fcf97fdb6aa0154
unknown <lopezi@PCSISING-ASU43.personal.com.py>
2015-06-22 17:22:14 -0400

    Implementacion-manual-PersonManager-sin-RIA
    - Cambios a la presentación

--- Index.php
+++ Index.php
@@ -3,21 +3,17 @@
 <head>
   <meta charset="utf-8" />
   <title>Home</title>
-<style>
-  body {background-color:#289e0c}
-  a    {color:#fff}
-  label    {color:#fff}   
-</style>
+  <link rel="stylesheet" type="text/css" href="Paneles de la aplicacion.css">
 </head>
 <body>
-
-    <a href="Index.php">New Person</a>
-    <a href="ListarPersonas.php">View Persons</a>
-    <a href="PresentacionRemover.php">Delete Person</a>
+    <h1 id="h101">Person Manager</h1>
+    <a href="Index.php">Formulario de ingreso</a>
+    <a href="ListarPersonas.php">Mostrar datos personales</a>
+    <a href="PresentacionRemover.php">Formulario de eliminacion</a>
  
     <form id = "signupForm" action="Agregar.php" method="POST">
     <fieldset>     
-    <legend>Por favor ingrese sus datos personales</legend>         
+         
 	<p>
 		<label for="firstname">Nombre:</label><br>
 		<input id="firstname" name="firstname" type="text" title="Ingresar nombre completo">
@@ -37,12 +33,12 @@
 	<fieldset>
 		<legend>Sexo</legend>
 		<label for="gender_male">
-			<input type="radio" id="gender_male" value="masculino" name="gender" required>Masculino
+			<input type="radio" id="gender_male" value="masculino" name="gender" >Masculino
 		</label>
 		<label for="gender_female">
 			<input type="radio" id="gender_female" value="femenino" name="gender">Femenino
 		</label>
-		<label for="gender" class="error">Please select your gender</label> 
+		
 	</fieldset>
 	<p>
 		<label for="email">Email:</label><br>
@@ -53,22 +49,15 @@
 		<input id="user" name="user" type="text">
 	</p> 
 	<p>
-		<label for="pass">Contraseña:</label><br>
+		<label for="pass">Clave:</label><br>
 		<input id="pass" name="pass" type="password" title="La contraseña debe contar con al menos 8 caracteres, Se recomienda ingresar caracteres alfanumericos combinados">
 	</p> 
 	<p>
-		<label for="confirm_pass">Confirmacion de contraseña:</label><br>
-		<input id="confirm_pass" name="confirm_pass" type="password">
-	</p> 
-	<p>
-		<label for="agree">Confirms that all entries are correct?</label>
-		<input type="checkbox" class="checkbox" id="agree" name="agree">
-	</p>
-	<p>
-		<input class="submit" type="submit" value="Submit">
+		<input class="submit" type="submit" value="Agregar">
 	</p>
     </fieldset>
     </form> 
-    
+    <p id="p01">Universidad Católica - 2015</p>        
+
 </body>
 </html>
